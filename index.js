891e0f4eea260b891ac90149a1e2568250b6f79e
Bobby Vasquez <shootbv@gmail.com>
2021-07-25 12:22:22 -0600

    Add basic get responses, app.listen

--- index.js
+++ index.js
@@ -8,3 +8,23 @@ app.use(express.json());
 
 app.locals.title = 'Better Jeopardy API';
 pool.connect();
+
+app.get('/', (request, response) => {
+  response.status(200).send('Welcome to the BetterJeopardy API')
+});
+
+app.get('/questions', (request, response) => {
+  let questions;
+  pool.query('SELECT * FROM question', (err, res) => {
+    // console.log(res)
+    if (err) {
+      console.log(err)
+    }
+    questions = res.rows;
+    response.status(200).send({questions})
+  })
+});
+
+app.listen(3001, () => {
+  console.log(`${app.local.title} has started on port 3001`)
+})
