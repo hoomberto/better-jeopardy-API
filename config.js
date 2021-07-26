4de7260f804da3a302db1bdc9860d18aabeb1433
Dean Cook <deancook371@gmail.com>
2021-07-26 09:15:27 -0700

    Update database URL in config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 
 const isProduction = process.env.NODE_ENV === 'production'
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+const connectionString = `postgres://xvkpuofcnndyjr:c3dc401551f597335bd3115c401af0f9c8bbcd8196227e30d40c34ec9fcebead@ec2-52-72-125-94.compute-1.amazonaws.com:5432/d5mfdsej9ljpfe`
 
 
 const pool = new Pool({
